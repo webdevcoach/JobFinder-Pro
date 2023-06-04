66d6fc9345b19bff2c2e290777dc34a24e96c058
Gideon Acromond <acromondx@gmail.com>
2023-06-04 17:58:44 +0000

    New recruiter homeview card

--- lib/features/recruiter/features/home/views/recruiter_home.dart
+++ lib/features/recruiter/features/home/views/recruiter_home.dart
@@ -6,6 +6,8 @@ import 'package:jobhunt_pro/features/authentication/controller/auth_controller.d
 import 'package:jobhunt_pro/features/recruiter/features/post_job/views/view_applicants/apllicant_details.dart';
 import 'package:jobhunt_pro/theme/colors.dart';
 
+import '../../../../../common/info_chip.dart';
+import '../../../../../common/verticalbar_decoration.dart';
 import '../../../../applicant/features/apply_job/controller/apply_job_conntroller.dart';
 
 class RecruiterHomeView extends ConsumerWidget {
@@ -13,8 +15,6 @@ class RecruiterHomeView extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, ref) {
-    final txtStyle = Theme.of(context).textTheme.displayMedium;
-
     final currentUser = ref.watch(currentRecruiterDetailsProvider).value;
 
     if (currentUser == null) {
@@ -27,14 +27,8 @@ class RecruiterHomeView extends ConsumerWidget {
         appBar: AppBar(
           title: Row(
             children: [
-              const CircleAvatar(
-                radius: 20,
-              ),
-              const SizedBox(width: 10),
-              Text(
-                'Hi there 👋🏽 ${currentUser.companyName}',
-                style: txtStyle!.copyWith(fontSize: 13),
-              ),
+              // const SizedBox(width: 10),
+              const Text('Hi there 👋🏽'),
               const Spacer(),
               IconButton(
                   onPressed: () {},
@@ -48,44 +42,84 @@ class RecruiterHomeView extends ConsumerWidget {
             ],
           ),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(12),
-          child: ref.watch(applicantListProvider).when(
-              data: (applicant) {
-                return ListView.builder(
-                    itemCount: applicant.length,
-                    itemBuilder: (context, index) {
-                      final details = applicant[index];
-                      return ListTile(
-                        leading: CircleAvatar(
-                          backgroundImage: NetworkImage(
-                            details.profilePicture.isEmpty
-                                ? 'https://i.pravatar.cc/300?img=60'
-                                : details.profilePicture,
+        body: ListView(
+          padding: const EdgeInsets.all(10),
+          children: [
+            VerticalBar(title: 'Trending'),
+            ref.watch(applicantListProvider).when(
+                data: (applicant) {
+                  return ListView.separated(
+                      separatorBuilder: (context, index) =>
+                          const SizedBox(height: 15),
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: applicant.length,
+                      itemBuilder: (context, index) {
+                        final details = applicant[index];
+                        return InkWell(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        ApplicantDetails(applicant: details)));
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                boxShadow: const [
+                                  BoxShadow(
+                                      color: AppColors.secondaryColor,
+                                      blurStyle: BlurStyle.outer,
+                                      blurRadius: 1)
+                                ]),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                ListTile(
+                                  leading: CircleAvatar(
+                                    radius: 30,
+                                    backgroundImage: NetworkImage(
+                                      details.profilePicture.isEmpty
+                                          ? 'https://i.pravatar.cc/300?img=60'
+                                          : details.profilePicture,
+                                    ),
+                                  ),
+                                  title: CustomText(
+                                      text: details.name, bold: true, size: 17),
+                                  subtitle:
+                                      CustomText(text: details.title, size: 14),
+                                  trailing:
+                                      const Icon(IconlyLight.arrow_right_2),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 10.0),
+                                  child: Wrap(
+                                    spacing: 10.0,
+                                    runSpacing: 10,
+                                    children: details.skills
+                                        .map((item) => InfoChip(
+                                              title: '#$item',
+                                              titleColor: Colors.blue.shade900,
+                                              bold: false,
+                                            ))
+                                        .toList(),
+                                  ),
+                                ),
+                                const SizedBox(height: 10)
+                              ],
+                            ),
                           ),
-                        ),
-                        title: CustomText(
-                            text: details.name, bold: true, size: 17),
-                        subtitle: CustomText(
-                            text: details.title,
-                            size: 14,
-                            color: AppColors.primaryColor),
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      ApplicantDetails(applicant: details)));
-                        },
-                        trailing: const Icon(IconlyLight.arrow_right_2),
-                      );
-                    });
-              },
-              error: (error, stackTrace) =>
-                  Center(child: Text(error.toString())),
-              loading: () => const Center(
-                    child: CircularProgressIndicator.adaptive(),
-                  )),
+                        );
+                      });
+                },
+                error: (error, stackTrace) =>
+                    Center(child: Text(error.toString())),
+                loading: () => const Center(
+                      child: CircularProgressIndicator.adaptive(),
+                    )),
+          ],
         ));
   }
 }
